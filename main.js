f6f2dee139be3fb62735a3cd57259ac371731d60
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-30 01:07:15 +0600

    added

---
+++ main.js
@@ -0,0 +1,17 @@
+var elements = document.getElementsByClassName("product");
+// var listView = document.getElementById("list-view");
+// var gridView = document.getElementById("grid-view");
+
+// listView.addEventListener("click", function(){
+//     var elements = document.getElementsByClassName("product");
+//     for(var i = 0; i<elements.length;i++)
+//     {
+//         elements[i].style.width = "100%"
+//     }
+// })
+var i;
+function listView() {
+    for (i = 0; i < elements.length; i++) {
+      elements[0].style.width = "100%";
+    }
+}
